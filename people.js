c020078335e7caf0caf09ae166a8b619ab661503
fabiodagostino27 <fabiodago27@gmail.com>
2025-02-06 12:30:22 +0100

    Exercise done

--- people.js
+++ people.js
@@ -1,2 +1,11 @@
 const fullName = require("./names.js");
 const hobbies = require("./hobbies.js");
+
+const person = () => {
+    return {
+        name: fullName("John", "Doe"),
+        hobbies: hobbies("football", "movies", "videogames")
+    }
+}
+
+console.log(person())
